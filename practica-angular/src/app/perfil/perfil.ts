86d2bdffe4197cd71abdac5822abc371a9333b88
Clearminds Dev <bowenjoaquinsd@gmail.com>
2026-05-10 18:30:33 -0500

    Multiples componentes

--- practica-angular/src/app/perfil/perfil.ts
+++ practica-angular/src/app/perfil/perfil.ts
@@ -1,8 +1,12 @@
 import { Component } from "@angular/core";
+import { Cuenta } from "../cuenta/cuenta";
+import { Transacciones } from "../transacciones/transacciones";
+import { Prestamos } from "../prestamos/prestamos";
 
 @Component(
     {
         selector: "mi-perfil",
+        imports: [Cuenta, Transacciones, Prestamos],
         templateUrl: "perfil.html",
         styleUrl: "perfil.css"
     }
